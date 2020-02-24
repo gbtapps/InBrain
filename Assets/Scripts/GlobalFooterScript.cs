b91fa225506c13053e0767ee76218968de2f75a5
moritomi <yutaka.moritomi@gmail.com>
2020-02-24 12:24:40 +0900

    縦型画面のプロトタイプ実装。横型と未統合。

---
+++ Assets/Scripts/GlobalFooterScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobalFooterScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToHome()
+    {
+        SceneManager.LoadScene("03.Home");        
+    }
+
+    public void MoveToAnalysis()
+    {
+        SceneManager.LoadScene("04.ConnectSensor");
+    }
+    public void MoveToBrainEx()
+    {
+        SceneManager.LoadScene("20.BrainExMenu");
+    }
+    public void MoveToInfo()
+    {
+        SceneManager.LoadScene("30.Info");
+    }
+    public void MoveToMyPage()
+    {
+        SceneManager.LoadScene("40.MyPage");
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/P01SignInControllerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class P01SignInControllerScript : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToSignUp()
+    {
+
+        SceneManager.LoadScene("02.SignUp");
+    }
+
+
+}
