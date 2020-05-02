8b6ac0055959419c140fd75700d1633fa97e7156
moritomi <yutaka.moritomi@gmail.com>
2020-05-02 19:19:05 +0900

    050203＞TrainingSettingにカウントダウンを追加して次シーンへ遷移

--- Assets/Script/Scene/MFN/Tr_TraningSetting.cs
+++ Assets/Script/Scene/MFN/Tr_TraningSetting.cs
@@ -8,6 +8,8 @@ public class Tr_TraningSetting : SceneBase
     //実行するトレーニング
     static ConstData.EnumScene playTraining;
 
+
+
     public static void SetPlayTraining(ConstData.EnumScene _playTraining)
     {
         playTraining = _playTraining;
@@ -50,9 +52,66 @@ public class Tr_TraningSetting : SceneBase
 
         if (btnStart.lastHit2)
         {
+            //これを呼び出す
             SceneFunc.ChangeScene(playTraining, false);
         }
+
+        //added by moritomi 
+        UpdateCountDownAndStartTest();
+
     }
+
+    /* added by moritomi from here*/
+    // Parameters
+    int CountTime;
+    float SumDeltTime;
+    int IntervalSeconds = 5;
+    [SerializeField] Text CountDownText;
+
+    // 10 seconds count down and start the function of change scene on the count of zero. 
+    private void UpdateCountDownAndStartTest()
+    {
+
+        //DeltaTime 
+        SumDeltTime += Time.deltaTime;
+        if( SumDeltTime > 1)
+        {
+            Debug.Log(IntervalSeconds);
+            CountDownText.text = IntervalSeconds.ToString();
+
+            IntervalSeconds--;
+            SumDeltTime = 0;
+
+            if (IntervalSeconds == 0)
+            {
+                Debug.Log("Zero");
+                SceneFunc.ChangeScene(playTraining, false);
+
+
+            }
+        }
+
+
+        /*
+        if(CountTime % 1 == 0)
+        {
+            Debug.Log(CountTime);
+
+        }
+        */
+
+
+        /*
+        for (int i=MAXCOUNT; i>-1; i--)
+        {
+            
+        }
+        */
+
+
+    }
+
+    /* added by moritomi until here*/
 
 
 
